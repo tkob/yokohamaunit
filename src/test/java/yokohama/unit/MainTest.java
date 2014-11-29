6b77cd6ea75ecc2807e852050713d09d7210ca11
tkob <ether4@gmail.com>
2014-11-29 13:36:30 +0900

    Added missing static import

--- src/test/java/yokohama/unit/MainTest.java
+++ src/test/java/yokohama/unit/MainTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
