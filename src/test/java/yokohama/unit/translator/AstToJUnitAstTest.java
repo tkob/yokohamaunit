6c03bee36919aaca4a7d234f563cd4177aad3f97
tkob <ether4@gmail.com>
2014-12-06 20:26:10 +0900

    Fix arguments for TestMethod constructor

--- src/test/java/yokohama/unit/translator/AstToJUnitAstTest.java
+++ src/test/java/yokohama/unit/translator/AstToJUnitAstTest.java
@@ -79,7 +79,7 @@ public class AstToJUnitAstTest {
         List<Table> tables = Arrays.asList();
         AstToJUnitAst instance = new AstToJUnitAst();
         List<TestMethod> actual = instance.translateAssertion(assertion, 0, testName, tables);
-        List<TestMethod> expected = Arrays.asList(new TestMethod("test_0", Arrays.asList(), Arrays.asList()));
+        List<TestMethod> expected = Arrays.asList(new TestMethod("test_0", Arrays.asList(), Arrays.asList(), Arrays.asList(), Arrays.asList()));
         assertThat(actual, is(expected));
     }
 
