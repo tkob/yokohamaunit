bb33fe1fae94ed33226bdf57e9e0ee25792a19f1
tkob <ether4@gmail.com>
2014-12-06 13:05:00 +0900

    Fix: FourPhaseTest should be a subtype of Definition

--- src/main/java/yokohama/unit/ast/FourPhaseTest.java
+++ src/main/java/yokohama/unit/ast/FourPhaseTest.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 import lombok.Value;
 
 @Value
-public class FourPhaseTest {
+public class FourPhaseTest implements Definition {
     private int numHashes;
     private String name;
     private Optional<Phase> setup;
     private Optional<Phase> exercise;
     private Optional<Phase> verify;
     private Optional<Phase> teardown;
+
+    @Override
+    public <T> T accept(DefinitionVisitor<T> visitor) {
+        return visitor.visitFourPhaseTest(this);
+    }
 }
