1bd01b6da13c26e6c44a1bd1190881da79e75558
tkob <ether4@gmail.com>
2014-12-06 22:13:34 +0900

    Remove visitAssertion and visitLetBindings since they are not subtypes of Action now

--- src/main/java/yokohama/unit/ast/ActionVisitor.java
+++ src/main/java/yokohama/unit/ast/ActionVisitor.java
@@ -1,7 +1,5 @@
 package yokohama.unit.ast;
 
 public interface ActionVisitor<T> {
-    T visitAssertion(Assertion assertion);
-    T visitLetBindings(LetBindings letBindings);
     T visitExecution(Execution execution);
 }
