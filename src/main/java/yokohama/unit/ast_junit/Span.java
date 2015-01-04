d1e1ebc70921dd92f1e25952ba4663d5245b6e72
tkob <ether4@gmail.com>
2015-01-04 09:30:36 +0900

    Add dummySpan static method

--- src/main/java/yokohama/unit/ast_junit/Span.java
+++ src/main/java/yokohama/unit/ast_junit/Span.java
@@ -10,4 +10,13 @@ public class Span {
     private final Optional<Path> sourcePath;
     private final Position start;
     private final Position end;
+
+    private static Span dummySpan =
+            new Span(
+                    Optional.empty(),
+                    new Position(-1, -1),
+                    new Position(-1, -1));
+    public static Span dummySpan() {
+        return dummySpan;
+    }
 }
