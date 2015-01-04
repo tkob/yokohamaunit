67de87e07d089a45a17d1b7dc14f2d11c5fc5fd6
tkob <ether4@gmail.com>
2015-01-04 22:19:21 +0900

    Remove an unused import statement

--- src/main/java/yokohama/unit/ast_junit/OgnlExpressionStrategy.java
+++ src/main/java/yokohama/unit/ast_junit/OgnlExpressionStrategy.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
-import lombok.AllArgsConstructor;
 import static org.apache.commons.lang3.StringEscapeUtils.escapeJava;
 import yokohama.unit.util.SBuilder;
 
