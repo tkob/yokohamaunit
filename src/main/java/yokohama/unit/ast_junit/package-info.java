cefeaf1dde4ae29c077fc712a6677ff68a1030f7
tkob <ether4@gmail.com>
2015-01-10 10:27:46 +0900

    Update JUnit AST documentation

--- src/main/java/yokohama/unit/ast_junit/package-info.java
+++ src/main/java/yokohama/unit/ast_junit/package-info.java
@@ -8,9 +8,27 @@
  * 
  * TestMethod ::= Binding* ActionStatement* TestStateMent* ActionStatement*
  * 
+ * Binding ::= Expr
+ * 
+ * Expr ::= QuotedExpr | StubExpr
+ * 
+ * StubExpr ::= QuotedExpr StubBehavior
+ * StubBehavior ::= MethodPattern Expr
+ * MethodPattern ::= Type*
+ * Type ::= NonArrayType
+ * NonArrayType ::= PrimitiveType | ClassType
+ * PrimitiveType ::= Kind
+ * 
+ * ActionStatement ::= QuotedExpr
+ * 
  * TestStatement ::= IsStatement
  *                 | IsNotStatement
  *                 | ThrowsStatement
+ * 
+ * IsStatement ::= QuotedExpr QuotedExpr
+ * IsNotStatement ::= QuotedExpr QuotedExpr
+ * ThrowsStatement ::= QuotedExpr QuotedExpr
+ * 
  * </pre>
  * 
  */
