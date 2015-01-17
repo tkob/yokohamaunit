02d4842fd9a8c03fe19d1e22e5b9a61c108bf00f
tkob <ether4@gmail.com>
2015-01-17 22:51:07 +0900

    Change class visibility to public

--- src/main/java/yokohama/unit/ast_junit/StatementVisitor.java
+++ src/main/java/yokohama/unit/ast_junit/StatementVisitor.java
@@ -1,6 +1,6 @@
 package yokohama.unit.ast_junit;
 
-interface StatementVisitor<T> {
+public interface StatementVisitor<T> {
     T visitIsStatement(IsStatement isStatement);
     T visitIsNotStatement(IsNotStatement isNotStatement);
     T visitActionStatement(ActionStatement actionStatement);
