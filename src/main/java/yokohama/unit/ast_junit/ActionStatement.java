122b808248380906e59f7adeec4f99a5577e9074
tkob <ether4@gmail.com>
2015-01-17 22:50:41 +0900

    Add @Override annotation

--- src/main/java/yokohama/unit/ast_junit/ActionStatement.java
+++ src/main/java/yokohama/unit/ast_junit/ActionStatement.java
@@ -8,10 +8,12 @@ import yokohama.unit.util.SBuilder;
 public class ActionStatement implements Statement {
     private QuotedExpr action;
 
+    @Override
     public Set<ImportedName> importedNames(ExpressionStrategy expressionStrategy, MockStrategy mockStrategy) {
         return expressionStrategy.getValueImports();
     }
 
+    @Override
     public void toString(SBuilder sb, ExpressionStrategy expressionStrategy, MockStrategy mockStrategy) {
         sb.appendln(expressionStrategy.getValue(action), ";");
     }
