c26380689df144c6b6de5659f75f2f23470b8c2a
tkob <ether4@gmail.com>
2015-01-17 22:39:42 +0900

    Replace TreeSet with HashSet

--- src/main/java/yokohama/unit/util/SetUtils.java
+++ src/main/java/yokohama/unit/util/SetUtils.java
@@ -1,13 +1,13 @@
 package yokohama.unit.util;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 import lombok.NonNull;
 
 public class SetUtils {
     public static <E> Set<E> union(@NonNull Collection<? extends E> a, @NonNull Collection<? extends E> b) {
-        Set<E> s = new TreeSet<>();
+        Set<E> s = new HashSet<>();
         s.addAll(a);
         s.addAll(b);
         return s;
