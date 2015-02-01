1d5e02be62afba60c51daf82b953aa027b2a0e03
tkob <ether4@gmail.com>
2015-02-01 21:22:56 +0900

    Add EqualToMatcherExpr class

---
+++ src/main/java/yokohama/unit/ast_junit/EqualToMatcherExpr.java
@@ -0,0 +1,23 @@
+package yokohama.unit.ast_junit;
+
+import java.util.Set;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import yokohama.unit.util.SBuilder;
+import static yokohama.unit.util.SetUtils.setOf;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class EqualToMatcherExpr extends MatcherExpr {
+    private Var operand;
+
+    @Override
+    public void getExpr(SBuilder sb, String varName, ExpressionStrategy expressionStrategy) {
+        sb.appendln("Matcher ", varName, " = is(", operand.getName(), ");");
+    }
+
+    @Override
+    public Set<ImportedName> importedNames() {
+        return setOf(new ImportStatic("org.hamcrest.CoreMatchers.is"));
+    }
+}
