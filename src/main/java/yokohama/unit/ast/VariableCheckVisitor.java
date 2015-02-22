691714c25f0ba4010c251ea50fcb3d3e2c7d44d2
tkob <ether4@gmail.com>
2015-02-22 22:47:14 +0900

    Delete unused import

--- src/main/java/yokohama/unit/ast/VariableCheckVisitor.java
+++ src/main/java/yokohama/unit/ast/VariableCheckVisitor.java
@@ -1,7 +1,6 @@
 package yokohama.unit.ast;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import yokohama.unit.util.FList;
