8ce4ab231d8fc6eaeb8f8b7ead12178bdc49e47e
tkob <ether4@gmail.com>
2015-03-02 21:36:06 +0900

    Create BcelJUnitAstCompiler (yet not implemented) and test

---
+++ src/main/java/yokohama/unit/translator/BcelJUnitAstCompiler.java
@@ -0,0 +1,27 @@
+package yokohama.unit.translator;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import yokohama.unit.ast_junit.CompilationUnit;
+
+public class BcelJUnitAstCompiler implements JUnitAstCompiler {
+    @Override
+    public boolean compile(
+            CompilationUnit ast,
+            String className,
+            String packageName,
+            List<String> classPath,
+            Optional<Path> dest,
+            List<String> javacArgs) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Path makeClassFilePath(Optional<Path> dest, String packageName, String className) {
+        Path classFile = (dest.isPresent() ? dest.get(): Paths.get("."))
+                .resolve(Paths.get(packageName.replace('.', '/')))
+                .resolve(className + ".class");
+        return classFile;
+    }
+}
