368229429890553431b68fefd7d26d6f9ad69eba
tkob <ether4@gmail.com>
2015-03-07 11:17:48 +0900

    Add utility constructor that sets dummy span

--- src/main/java/yokohama/unit/ast_junit/ClassType.java
+++ src/main/java/yokohama/unit/ast_junit/ClassType.java
@@ -7,6 +7,16 @@ public class ClassType implements NonArrayType {
     private String name;
     private Span span;
 
+    public ClassType(String name, Span span) {
+        this.name = name;
+        this.span = span;
+    }
+
+    public ClassType(String name) {
+        this.name = name;
+        this.span = Span.dummySpan();
+    }
+
     @Override
     public <T> T accept(NonArrayTypeVisitor<T> visitor) {
         return visitor.visitClassType(this);
