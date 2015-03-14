54e044c977856ad256299c2f9b835e9ca12131c9
tkob <ether4@gmail.com>
2015-03-14 22:10:25 +0900

    Add INT constant

--- src/main/java/yokohama/unit/ast_junit/Type.java
+++ src/main/java/yokohama/unit/ast_junit/Type.java
@@ -2,6 +2,7 @@ package yokohama.unit.ast_junit;
 
 import lombok.Value;
 import org.apache.commons.lang3.StringUtils;
+import yokohama.unit.ast.Kind;
 
 @Value
 public class Type {
@@ -12,6 +13,7 @@ public class Type {
     public static final Type THROWABLE = new Type(new ClassType("java.lang.Throwable", Span.dummySpan()), 0);
     public static final Type STRING = new Type(new ClassType("java.lang.String", Span.dummySpan()), 0);
     public static final Type MATCHER = new Type(new ClassType("org.hamcrest.Matcher", Span.dummySpan()), 0);
+    public static final Type INT = new Type(new PrimitiveType(Kind.INT), 0);
 
     public String getText() {
         return nonArrayType.getText() + StringUtils.repeat("[]", dims);
