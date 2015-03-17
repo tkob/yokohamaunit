a0514abd28901b1e80f37d857402959e19f0323f
tkob <ether4@gmail.com>
2015-03-17 22:32:50 +0900

    Add primitive types

--- src/main/java/yokohama/unit/ast_junit/Type.java
+++ src/main/java/yokohama/unit/ast_junit/Type.java
@@ -14,7 +14,14 @@ public class Type {
     public static final Type CLASS = new Type(new ClassType("java.lang.Class", Span.dummySpan()), 0);
     public static final Type STRING = new Type(new ClassType("java.lang.String", Span.dummySpan()), 0);
     public static final Type MATCHER = new Type(new ClassType("org.hamcrest.Matcher", Span.dummySpan()), 0);
+    public static final Type BOOLEAN = new Type(new PrimitiveType(Kind.BOOLEAN), 0);
+    public static final Type BYTE = new Type(new PrimitiveType(Kind.BYTE), 0);
+    public static final Type SHORT = new Type(new PrimitiveType(Kind.SHORT), 0);
     public static final Type INT = new Type(new PrimitiveType(Kind.INT), 0);
+    public static final Type LONG = new Type(new PrimitiveType(Kind.LONG), 0);
+    public static final Type CHAR = new Type(new PrimitiveType(Kind.CHAR), 0);
+    public static final Type FLOAT = new Type(new PrimitiveType(Kind.FLOAT), 0);
+    public static final Type DOUBLE = new Type(new PrimitiveType(Kind.DOUBLE), 0);
 
     public String getText() {
         return nonArrayType.getText() + StringUtils.repeat("[]", dims);
