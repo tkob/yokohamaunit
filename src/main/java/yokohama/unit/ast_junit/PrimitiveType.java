5b81f28f3782a64a16e4b3b408cb95e01aef9e32
tkob <ether4@gmail.com>
2015-03-18 21:35:37 +0900

    Add box method

--- src/main/java/yokohama/unit/ast_junit/PrimitiveType.java
+++ src/main/java/yokohama/unit/ast_junit/PrimitiveType.java
@@ -12,6 +12,28 @@ public class PrimitiveType implements NonArrayType {
         return kind.name().toLowerCase();
     }
 
+    public ClassType box() {
+        switch (kind) {
+            case BOOLEAN:
+                return new ClassType("java.lang.Boolean", Span.dummySpan());
+            case BYTE:
+                return new ClassType("java.lang.Byte", Span.dummySpan());
+            case SHORT:
+                return new ClassType("java.lang.Short", Span.dummySpan());
+            case INT:
+                return new ClassType("java.lang.Integer", Span.dummySpan());
+            case LONG:
+                return new ClassType("java.lang.Long", Span.dummySpan());
+            case CHAR:
+                return new ClassType("java.lang.Character", Span.dummySpan());
+            case FLOAT:
+                return new ClassType("java.lang.Float", Span.dummySpan());
+            case DOUBLE:
+                return new ClassType("java.lang.Double", Span.dummySpan());
+        }
+        throw new RuntimeException("should not reach here");
+    }
+
     @Override
     public <T> T accept(NonArrayTypeVisitor<T> visitor) {
         return visitor.visitPrimitiveType(this);
