ae26fcf486ca489b573a1f86c02f784fe58657ee
tkob <ether4@gmail.com>
2015-04-19 21:10:27 +0900

    Add toPair method to Binding class

--- src/main/java/yokohama/unit/ast/Binding.java
+++ src/main/java/yokohama/unit/ast/Binding.java
@@ -2,6 +2,7 @@ package yokohama.unit.ast;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import yokohama.unit.util.Pair;
 
 @Value
 @EqualsAndHashCode(exclude={"span"})
@@ -9,4 +10,8 @@ public class Binding {
     private Ident name;
     private Expr value;
     private Span span;
+
+    public Pair<Ident, Expr> toPair() {
+        return new Pair<>(name, value);
+    }
 }
