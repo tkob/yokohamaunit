ed67c442ffeba1b5b88ce7e35c52e20126226b95
tkob <ether4@gmail.com>
2015-04-20 22:25:36 +0900

    Add Position.of method

--- src/main/java/yokohama/unit/position/Position.java
+++ src/main/java/yokohama/unit/position/Position.java
@@ -7,6 +7,14 @@ public class Position {
     private int line;
     private int column;
 
+    public static Position of(int line) {
+        return new Position(line, -1);
+    }
+
+    public static Position of(int line, int column) {
+        return new Position(line, column);
+    }
+
     private static Position dummyPos = new Position(-1, -1);
     public static Position dummyPos() {
         return dummyPos;
