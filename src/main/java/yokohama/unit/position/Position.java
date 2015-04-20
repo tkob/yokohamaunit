0382d813cbc3c4b5545a8bd9ecac33044b0154ff
tkob <ether4@gmail.com>
2015-04-20 22:36:32 +0900

    Add Position.toString method and test

--- src/main/java/yokohama/unit/position/Position.java
+++ src/main/java/yokohama/unit/position/Position.java
@@ -10,6 +10,17 @@ public class Position {
     public boolean isDummy() {
         return line < 0 && column < 0;
     }
+ 
+    @Override
+    public String toString() {
+        if (line < 0) {
+            return "?";
+        } else if (column < 0) {
+            return Integer.toString(line);
+        } else {
+            return line + "." + column;
+        }
+    }
 
     public static Position of(int line) {
         return new Position(line, -1);
