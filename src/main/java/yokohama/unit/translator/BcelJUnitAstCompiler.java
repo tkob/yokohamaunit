d45ddffc37446027f5e7540f80e566f847b9899e
tkob <ether4@gmail.com>
2015-04-21 21:56:44 +0900

    Remove unused import

--- src/main/java/yokohama/unit/translator/BcelJUnitAstCompiler.java
+++ src/main/java/yokohama/unit/translator/BcelJUnitAstCompiler.java
@@ -2,7 +2,6 @@ package yokohama.unit.translator;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
