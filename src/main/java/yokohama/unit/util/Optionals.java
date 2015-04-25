defd98d72b2438df651370afa27ec7b806b100f7
tkob <ether4@gmail.com>
2015-04-25 15:14:16 +0900

    Add toStream method to Optionals class

--- src/main/java/yokohama/unit/util/Optionals.java
+++ src/main/java/yokohama/unit/util/Optionals.java
@@ -3,9 +3,14 @@ package yokohama.unit.util;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Optionals {
     public static <T, U> U match(Optional<T> optional, Supplier<U> none, Function<? super T, U> some) {
         return optional.isPresent() ? some.apply(optional.get()) : none.get();
+    }
+
+    public static <T> Stream<T> toStream(Optional<T> optional) {
+        return optional.isPresent() ? Stream.of(optional.get()) : Stream.empty();
     }
 }
