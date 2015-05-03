0f4cb36d7d2e907fb394d93f88a46130685178a5
tkob <ether4@gmail.com>
2015-05-03 20:34:12 +0900

    Add Pair.zip method

--- src/main/java/yokohama/unit/util/Pair.java
+++ src/main/java/yokohama/unit/util/Pair.java
@@ -1,9 +1,21 @@
 package yokohama.unit.util;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.Value;
 
 @Value
 public class Pair<T, U> {
     private final T first;
     private final U second;
+
+    public static <T, U> List<Pair<T, U>> zip(List<T> firsts, List<U> seconds) {
+        if (firsts.size() != seconds.size())
+            throw new IllegalArgumentException("List size not match");
+
+        return IntStream.range(0, firsts.size())
+                .mapToObj(i -> new Pair<T, U>(firsts.get(i), seconds.get(i)))
+                .collect(Collectors.toList());
+    }
 }
