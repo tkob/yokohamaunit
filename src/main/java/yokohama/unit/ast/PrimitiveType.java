8961c783dea10a3ba16a7205b730243c1642b769
tkob <ether4@gmail.com>
2015-05-05 10:34:29 +0900

    Add fromType method to ast.Type and primitive type constants to Type and PrimitiveType

--- src/main/java/yokohama/unit/ast/PrimitiveType.java
+++ src/main/java/yokohama/unit/ast/PrimitiveType.java
@@ -10,6 +10,15 @@ public class PrimitiveType implements NonArrayType {
     private Kind kind;
     private Span span;
 
+    public static final PrimitiveType BOOLEAN = new PrimitiveType(Kind.BOOLEAN, Span.dummySpan());
+    public static final PrimitiveType BYTE = new PrimitiveType(Kind.BYTE, Span.dummySpan());
+    public static final PrimitiveType SHORT = new PrimitiveType(Kind.SHORT, Span.dummySpan());
+    public static final PrimitiveType INT = new PrimitiveType(Kind.INT, Span.dummySpan());
+    public static final PrimitiveType LONG = new PrimitiveType(Kind.LONG, Span.dummySpan());
+    public static final PrimitiveType CHAR = new PrimitiveType(Kind.CHAR, Span.dummySpan());
+    public static final PrimitiveType FLOAT = new PrimitiveType(Kind.FLOAT, Span.dummySpan());
+    public static final PrimitiveType DOUBLE = new PrimitiveType(Kind.DOUBLE, Span.dummySpan());
+
     @Override
     public <T> T accept(NonArrayTypeVisitor<T> visitor) {
         return visitor.visitPrimitiveType(this);
--- src/main/java/yokohama/unit/ast/Type.java
+++ src/main/java/yokohama/unit/ast/Type.java
@@ -11,7 +11,41 @@ public class Type {
     private int dims;
     private Span span;
 
+    public static final Type BOOLEAN = new Type(PrimitiveType.BOOLEAN, 0, Span.dummySpan());
+    public static final Type BYTE = new Type(PrimitiveType.BYTE, 0, Span.dummySpan());
+    public static final Type SHORT = new Type(PrimitiveType.SHORT, 0, Span.dummySpan());
+    public static final Type INT = new Type(PrimitiveType.INT, 0, Span.dummySpan());
+    public static final Type LONG = new Type(PrimitiveType.LONG, 0, Span.dummySpan());
+    public static final Type CHAR = new Type(PrimitiveType.CHAR, 0, Span.dummySpan());
+    public static final Type FLOAT = new Type(PrimitiveType.FLOAT, 0, Span.dummySpan());
+    public static final Type DOUBLE = new Type(PrimitiveType.DOUBLE, 0, Span.dummySpan());
+
     public Type toArray() {
         return new Type(nonArrayType, dims + 1, span);
+    }
+
+    public static Type fromClass(Class<?> clazz) {
+        if (clazz.isArray()) {
+            Type componentType = fromClass(clazz.getComponentType());
+            return new Type(
+                    componentType.getNonArrayType(),
+                    componentType.getDims() + 1,
+                    Span.dummySpan());
+        } else if (clazz.isPrimitive()) {
+            if      (clazz.equals(  Boolean.TYPE)) { return BOOLEAN; }
+            else if (clazz.equals(     Byte.TYPE)) { return BYTE;    }
+            else if (clazz.equals(    Short.TYPE)) { return SHORT;   }
+            else if (clazz.equals(  Integer.TYPE)) { return INT;     }
+            else if (clazz.equals(     Long.TYPE)) { return LONG;    }
+            else if (clazz.equals(Character.TYPE)) { return CHAR;    }
+            else if (clazz.equals(    Float.TYPE)) { return FLOAT;   }
+            else if (clazz.equals(   Double.TYPE)) { return DOUBLE;  }
+            else { throw new RuntimeException("should not reach here"); }
+        } else {
+            return new Type(
+                    new ClassType(clazz.getCanonicalName(), Span.dummySpan()),
+                    0,
+                    Span.dummySpan());
+        }
     }
 }
