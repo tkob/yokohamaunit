54d54f8cbc2ec930fbd3b25fd388f38bc4d822a3
tkob <ether4@gmail.com>
2015-05-10 00:22:26 +0900

    Add getCode method

--- src/main/java/yokohama/unit/ast/CodeBlock.java
+++ src/main/java/yokohama/unit/ast/CodeBlock.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import yokohama.unit.position.Span;
+import yokohama.unit.util.Lists;
 
 @Value
 @EqualsAndHashCode(exclude={"span"})
@@ -12,6 +13,25 @@ public class CodeBlock implements Definition {
     String lang;
     List<String> lines;    
     Span span;
+
+    public String getCode(
+            String lineSeparator, boolean appendLineSeparatorAtTheEnd) {
+        StringBuilder sb = new StringBuilder();
+        Lists.mapInitAndLast(lines,
+                line -> {
+                    sb.append(line);
+                    sb.append(lineSeparator);
+                    return null;
+                },
+                line -> {
+                    sb.append(line);
+                    if (appendLineSeparatorAtTheEnd) {
+                        sb.append(lineSeparator);
+                    }
+                    return null;
+                });
+        return sb.toString();
+    }
 
     @Override
     public <T> T accept(DefinitionVisitor<T> visitor) {
