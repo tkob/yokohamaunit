7f7ebf6d0517e80492f4ace7da83f7871e5734e7
tkob <ether4@gmail.com>
2015-05-10 09:51:20 +0900

    Fix: use forEachOrderedInitAndLast instead of mapInitAndLast, which is not order preserving

--- src/main/java/yokohama/unit/ast/CodeBlock.java
+++ src/main/java/yokohama/unit/ast/CodeBlock.java
@@ -17,18 +17,16 @@ public class CodeBlock implements Definition {
     public String getCode(
             String lineSeparator, boolean appendLineSeparatorAtTheEnd) {
         StringBuilder sb = new StringBuilder();
-        Lists.mapInitAndLast(lines,
+        Lists.forEachOrderedInitAndLast(lines,
                 line -> {
                     sb.append(line);
                     sb.append(lineSeparator);
-                    return null;
                 },
                 line -> {
                     sb.append(line);
                     if (appendLineSeparatorAtTheEnd) {
                         sb.append(lineSeparator);
                     }
-                    return null;
                 });
         return sb.toString();
     }
