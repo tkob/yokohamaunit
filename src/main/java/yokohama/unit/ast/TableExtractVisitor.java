77d61147439c6a30cdcce02d75398afb7814d585
tkob <ether4@gmail.com>
2015-05-10 20:22:05 +0900

    Add extractMap method to TableExtractVisitor

--- src/main/java/yokohama/unit/ast/TableExtractVisitor.java
+++ src/main/java/yokohama/unit/ast/TableExtractVisitor.java
@@ -1,10 +1,19 @@
 package yokohama.unit.ast;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import yokohama.unit.util.Lists;
+import yokohama.unit.util.Pair;
 
 public class TableExtractVisitor {
+    public Map<String, Table> extractMap(Group group) {
+        List<Table> tables = extractTables(group);
+        return Lists.listToMap(
+                tables, table -> new Pair<>(table.getName(), table));
+    }
+
     public List<Table> extractTables(Group group) {
         List<Definition> definitions = group.getDefinitions();
         return definitions
