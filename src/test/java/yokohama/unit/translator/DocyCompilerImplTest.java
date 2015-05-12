964052ba18bffbcf0fe186caaffe356176cfe24b
tkob <ether4@gmail.com>
2015-05-12 20:49:33 +0900

    Verify number of test methods instead of verifying all names of test methods

--- src/test/java/yokohama/unit/translator/DocyCompilerImplTest.java
+++ src/test/java/yokohama/unit/translator/DocyCompilerImplTest.java
@@ -10,9 +10,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.apache.commons.io.FilenameUtils;
 import static org.hamcrest.CoreMatchers.is;
@@ -28,13 +25,11 @@ import org.junit.runner.RunWith;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 import org.junit.runners.JUnit4;
-import org.junit.runners.model.FrameworkMethod;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import yokohama.unit.ast.VariableCheckVisitor;
 import yokohama.unit.position.ErrorMessage;
 
 @RunWith(Theories.class)
@@ -43,232 +38,61 @@ public class DocyCompilerImplTest {
     public static class Fixture {
         public final String docy;
         public final ExpressionStrategyFactory expressionStrategyFactory;
-        public final List<String> methods;
+        public final long numMethods;
     }
 
     @DataPoints
     public static Fixture[] PARAMs = {
         new Fixture(
-                "OgnlTestIs.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("Simple_Arithmetic_1")),
+                "OgnlTestIs.docy", new OgnlExpressionStrategyFactory(), 1),
         new Fixture(
-                "OgnlTestIsNot.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("Simple_Arithmetic_1")),
+                "OgnlTestIsNot.docy", new OgnlExpressionStrategyFactory(), 1),
         new Fixture(
-                "OgnlTestNull.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("Null_test_1", "Null_test_2")),
+                "OgnlTestNull.docy", new OgnlExpressionStrategyFactory(), 2),
         new Fixture(
-                "OgnlTestThrows.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Division_by_zero_1",
-                        "NPO_1",
-                        "OgnlException_1",
-                        "UnsupportedEncodingException_1",
-                        "No_exception_1")),
+                "OgnlTestThrows.docy", new OgnlExpressionStrategyFactory(), 5),
         new Fixture(
-                "OgnlTestMultiplePropositions.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("Multiple_propositions_1")),
+                "OgnlTestMultiplePropositions.docy", new OgnlExpressionStrategyFactory(), 1),
         new Fixture(
-                "OgnlTestMultipleAssertions.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("Multiple_assertions_1", "Multiple_assertions_2")),
+                "OgnlTestMultipleAssertions.docy", new OgnlExpressionStrategyFactory(), 2),
         new Fixture(
-                "OgnlTestBindings.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("String_startsWith_returns_true_if_the_prefix_is_empty_1")),
+                "OgnlTestBindings.docy", new OgnlExpressionStrategyFactory(), 1),
         new Fixture(
-                "OgnlTestTable.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList(
-                        "String_startsWith_1_1",
-                        "String_startsWith_1_2",
-                        "String_startsWith_1_3",
-                        "String_endsWith_1_1",
-                        "String_endsWith_1_2",
-                        "String_endsWith_1_3")),
+                "OgnlTestTable.docy", new OgnlExpressionStrategyFactory(), 6),
         new Fixture(
-                "OgnlTestCSV.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList(
-                        "String_startsWith_1_1",
-                        "String_startsWith_1_2",
-                        "String_startsWith_1_3")),
+                "OgnlTestCSV.docy", new OgnlExpressionStrategyFactory(), 3),
         new Fixture(
-                "OgnlTestExcel.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList(
-                        "String_startsWith_1_1",
-                        "String_startsWith_1_2",
-                        "String_startsWith_1_3")),
+                "OgnlTestExcel.docy", new OgnlExpressionStrategyFactory(), 3),
         new Fixture(
-                "OgnlTestFourPhase.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("AtomicInteger_incrementAndGet_increments_the_content")),
+                "OgnlTestFourPhase.docy", new OgnlExpressionStrategyFactory(), 1),
         new Fixture(
-                "OgnlTestFourPhaseWithTeardown.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("The_size_of_a_new_temporary_file_is_zero")),
+                "OgnlTestFourPhaseWithTeardown.docy", new OgnlExpressionStrategyFactory(), 1),
         new Fixture(
-                "OgnlTestStub.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Submit_a_task_and_get_the_result_1",
-                        "Collections_unmodifiableMap_preserves_lookup_1",
-                        "StringBuilder_append_CharSequence_int_int_calls_CharSequence_charAt")),
+                "OgnlTestStub.docy", new OgnlExpressionStrategyFactory(), 3),
         new Fixture(
-                "OgnlTestStubVariations.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList("Variations_of_stubbing_1")),
+                "OgnlTestStubVariations.docy", new OgnlExpressionStrategyFactory(), 1),
         new Fixture(
-                "OgnlTestInstanceSuchThat.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList(
-                        "instance_such_that_1",
-                        "instance_such_that_2",
-                        "instance_such_that_3",
-                        "instance_such_that_4")),
+                "OgnlTestInstanceSuchThat.docy", new OgnlExpressionStrategyFactory(), 4),
         new Fixture(
-                "OgnlTestImport.docy",
-                new OgnlExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Test_import_1",
-                        "Test_import_2",
-                        "Test_import_3",
-                        "Test_import_4")),
+                "OgnlTestImport.docy", new OgnlExpressionStrategyFactory(), 4),
         new Fixture(
-                "ElTestNull.docy",
-                new ElExpressionStrategyFactory(),
-                Arrays.asList("Null_test_1", "Null_test_2")),
+                "ElTestNull.docy", new ElExpressionStrategyFactory(), 2),
         new Fixture(
-                "ElTestThrows.docy",
-                new ElExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Division_by_zero_does_not_throw_an_Exception_in_EL_1",
-                        "Applying_null_to_a_method_does_not_throw_NPE_in_EL_1",
-                        "ElException_1",
-                        "IndexOutOfBoundsException_1",
-                        "No_exception_1")),
+                "ElTestThrows.docy", new ElExpressionStrategyFactory(), 5),
         new Fixture(
-                "ElTestImport.docy",
-                new ElExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Test_import_1",
-                        "Test_import_2",
-                        "Test_import_3",
-                        "Test_import_4")),
+                "ElTestImport.docy", new ElExpressionStrategyFactory(), 4),
         new Fixture(
-                "GroovyTestNull.docy",
-                new GroovyExpressionStrategyFactory(),
-                Arrays.asList("Null_test_1", "Null_test_2")),
+                "GroovyTestNull.docy", new GroovyExpressionStrategyFactory(), 2),
         new Fixture(
-                "GroovyTestThrows.docy",
-                new GroovyExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Division_by_zero_1",
-                        "Applying_null_to_toString_does_not_throw_NPE_in_Groovy_1",
-                        "GroovyRuntimeException_1",
-                        "UnsupportedEncodingException_1",
-                        "No_exception_1")),
+                "GroovyTestThrows.docy", new GroovyExpressionStrategyFactory(), 5),
         new Fixture(
-                "GroovyTestImport.docy",
-                new GroovyExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Test_import_1",
-                        "Test_import_2",
-                        "Test_import_3",
-                        "Test_import_4")),
+                "GroovyTestImport.docy", new GroovyExpressionStrategyFactory(), 4),
         new Fixture(
-                "GroovyTestLiterals.docy",
-                new GroovyExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Use_integer_literals_int_1",
-                        "Use_integer_literals_int_2",
-                        "Use_integer_literals_int_3",
-                        "Use_integer_literals_int_4",
-                        "Use_integer_literals_int_5",
-                        "Use_integer_literals_int_6",
-                        "Use_integer_literals_int_7",
-                        "Use_integer_literals_int_8",
-                        "Use_integer_literals_int_9",
-                        "Use_integer_literals_int_10",
-                        "Use_integer_literals_int_11",
-                        "Use_integer_literals_int_12",
-                        "Use_integer_literals_int_13",
-                        "Use_integer_literals_long_1",
-                        "Use_integer_literals_long_2",
-                        "Use_integer_literals_long_3",
-                        "Use_integer_literals_long_4",
-                        "Use_integer_literals_long_5",
-                        "Use_integer_literals_long_6",
-                        "Use_integer_literals_long_7",
-                        "Use_integer_literals_long_8",
-                        "Use_integer_literals_long_9",
-                        "Use_integer_literals_long_10",
-                        "Use_integer_literals_long_11",
-                        "Use_integer_literals_long_12",
-                        "Use_integer_literals_long_13",
-                        "Use_floating_point_literals_float_1",
-                        "Use_floating_point_literals_float_2",
-                        "Use_floating_point_literals_float_3",
-                        "Use_floating_point_literals_float_4",
-                        "Use_floating_point_literals_float_5",
-                        "Use_floating_point_literals_float_6",
-                        "Use_floating_point_literals_float_7",
-                        "Use_floating_point_literals_float_8",
-                        "Use_floating_point_literals_double_1",
-                        "Use_floating_point_literals_double_2",
-                        "Use_floating_point_literals_double_3",
-                        "Use_floating_point_literals_double_4",
-                        "Use_floating_point_literals_double_5",
-                        "Use_floating_point_literals_double_6",
-                        "Use_floating_point_literals_double_7",
-                        "Use_floating_point_literals_double_8",
-                        "Use_floating_point_literals_double_9",
-                        "Use_boolean_literals_1",
-                        "Use_boolean_literals_2",
-                        "Use_character_literals_1",
-                        "Use_character_literals_2",
-                        "Use_character_literals_3",
-                        "Use_character_literals_4",
-                        "Use_character_literals_5",
-                        "Use_character_literals_6",
-                        "Use_string_literals_1",
-                        "Use_string_literals_2",
-                        "Use_string_literals_3")),
+                "GroovyTestLiterals.docy", new GroovyExpressionStrategyFactory(), 54),
         new Fixture(
-                "GroovyTestInvocation.docy",
-                new GroovyExpressionStrategyFactory(),
-                Arrays.asList(
-                        "invokevirtual_with_no_argument_1",
-                        "invokevirtual_with_no_argument_2",
-                        "invokevirtual_with_no_argument_3",
-                        "invokevirtual_with_no_argument_4",
-                        "invokevirtual_with_no_argument_5",
-                        "List_remove_is_overloaded_on_arguments_1",
-                        "List_remove_is_overloaded_on_arguments_2",
-                        "invokestatic_with_no_argument_1",
-                        "Integer_valueOf_is_overloaded_1",
-                        "Integer_valueOf_is_overloaded_2",
-                        "Invoking_methods_with_vararg_1",
-                        "Invoking_methods_with_vararg_2",
-                        "Invoking_methods_with_vararg_3",
-                        "Invoking_methods_with_vararg_4")),
+                "GroovyTestInvocation.docy", new GroovyExpressionStrategyFactory(), 14),
         new Fixture(
-                "GroovyTestCodeBlock.docy",
-                new GroovyExpressionStrategyFactory(),
-                Arrays.asList(
-                        "Interpret_anchor_expression_1",
-                        "Back_ticks_inside_code_block_1",
-                        "Four_back_ticks_1",
-                        "Five_back_ticks_1",
-                        "Code_block_with_lf_1",
-                        "Code_block_with_crlf_1",
-                        "Code_block_with_chop_1")),
+                "GroovyTestCodeBlock.docy", new GroovyExpressionStrategyFactory(), 7),
     };
 
     @DataPoints
@@ -317,15 +141,13 @@ public class DocyCompilerImplTest {
             ClassLoader loader = new URLClassLoader(urls, this.getClass().getClassLoader());
             Class<?> klass = Class.forName(packageName + "." + className, true, loader);
 
-            // see if there are expected methods
+            // see if there are expected number of methods
             {
                 JUnit4 runner = new JUnit4(klass);
-                Set<String> actual =
+                long actual =
                         runner.getTestClass().getAnnotatedMethods(Test.class)
-                                .stream()
-                                .map(FrameworkMethod::getName)
-                                .collect(Collectors.toSet());
-                Set<String> expected = new TreeSet<>(fixture.methods);
+                                .stream().count();
+                long expected = fixture.numMethods;
                 assertThat(actual, is(expected));
             }
 
