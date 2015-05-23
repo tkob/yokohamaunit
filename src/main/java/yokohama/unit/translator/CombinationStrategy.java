557cd07647a243265ab7e95b0300fa1d58786d97
tkob <ether4@gmail.com>
2015-05-23 11:18:35 +0900

    Change return type of CombinationStrategy.generate to List<Map<K, V>>

--- src/main/java/yokohama/unit/translator/CombinationStrategy.java
+++ src/main/java/yokohama/unit/translator/CombinationStrategy.java
@@ -1,10 +1,11 @@
 package yokohama.unit.translator;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import yokohama.unit.util.Pair;
 
 public interface CombinationStrategy<K, V> {
-    Map<K, V> generate(Stream<Pair<K, Collection<V>>> candidates);
+    List<Map<K, V>> generate(Stream<Pair<K, Collection<V>>> candidates);
 }
--- src/main/java/yokohama/unit/translator/CombinationStrategyImpl.java
+++ src/main/java/yokohama/unit/translator/CombinationStrategyImpl.java
@@ -1,13 +1,14 @@
 package yokohama.unit.translator;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import yokohama.unit.util.Pair;
 
 public class CombinationStrategyImpl<K, V> implements CombinationStrategy<K, V> {
     @Override
-    public Map<K, V> generate(Stream<Pair<K, Collection<V>>> candidates) {
+    public List<Map<K, V>> generate(Stream<Pair<K, Collection<V>>> candidates) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
