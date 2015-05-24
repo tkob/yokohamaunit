724b035fb971cbab3cf2ef5c9697378a3cbb7c33
tkob <ether4@gmail.com>
2015-05-24 17:11:29 +0900

    Add DataConverterFinder and test

---
+++ src/test/java/yokohama/unit/translator/DataConverters.java
@@ -0,0 +1,11 @@
+package yokohama.unit.translator;
+
+import yokohama.unit.annotations.As;
+import yokohama.unit.ast.Group;
+
+@As
+public class DataConverters {
+    public Group asAst(String docy) {
+        throw new UnsupportedOperationException();
+    }
+}
