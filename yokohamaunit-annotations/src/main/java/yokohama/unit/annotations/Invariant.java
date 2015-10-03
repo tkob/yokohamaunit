531ab13491ca7201b58a2d1a5ce0f686e2e75982
tkob <ether4@gmail.com>
2015-10-03 12:24:03 +0900

    Make Invariant annotation repeatable

--- yokohamaunit-annotations/src/main/java/yokohama/unit/annotations/Invariant.java
+++ yokohamaunit-annotations/src/main/java/yokohama/unit/annotations/Invariant.java
@@ -1,8 +1,10 @@
 package yokohama.unit.annotations;
 
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Repeatable(Invariants.class)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Invariant {
     String value(); 
