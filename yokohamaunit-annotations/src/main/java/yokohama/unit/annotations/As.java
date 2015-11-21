15afeb2028b5161ebc00bf63f5e3b95d3d9bca77
tkob <ether4@gmail.com>
2015-11-21 15:30:11 +0900

    Ad RetentionPolicy

--- yokohamaunit-annotations/src/main/java/yokohama/unit/annotations/As.java
+++ yokohamaunit-annotations/src/main/java/yokohama/unit/annotations/As.java
@@ -1,4 +1,8 @@
 package yokohama.unit.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface As {
 }
