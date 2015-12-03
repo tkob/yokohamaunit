2710be57cee88a686147088f31611c3288be313a
tkob <ether4@gmail.com>
2015-12-03 22:30:42 +0900

    Remove unused constructor

--- src/main/java/yokohama/unit/util/GenSym.java
+++ src/main/java/yokohama/unit/util/GenSym.java
@@ -1,19 +1,12 @@
 package yokohama.unit.util;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class GenSym {
     private final Map<String, Integer> map = new HashMap<>();
 
     public GenSym() {}
-
-    public GenSym(List<String> reserved) {
-        for (String sym : reserved) {
-            generate(sym);
-        }
-    }
 
     public Sym generate(String prefix) {
         int index = map.compute(prefix, (k, v) -> v == null ? 1 : v + 1);
